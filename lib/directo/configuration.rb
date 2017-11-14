956c9d210408d48dad36d1e18352aaaa65225485
Artur Beljajev <artur.beljajev@internet.ee>
2017-11-14 19:20:21 +0200

    Add endpoint configuration
    #1

--- lib/directo/configuration.rb
+++ lib/directo/configuration.rb
@@ -1,5 +1,6 @@
 module Directo
   class Configuration
+    attr_accessor :endpoint
     attr_accessor :sales_agent
     attr_accessor :payment_terms
   end
--- test/configuration_test.rb
+++ test/configuration_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 module Directo
   class ConfigurationTest < Minitest::Test
+    def test_endpoint
+      Directo.configure { |config| config.endpoint = 'http://example.com' }
+      assert_equal 'http://example.com', Directo.configuration.endpoint
+    end
+
     def test_sales_agent
       Directo.configure { |config| config.sales_agent = 'John Doe' }
       assert_equal 'John Doe', Directo.configuration.sales_agent
